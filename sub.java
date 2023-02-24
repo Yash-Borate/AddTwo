dbe62d5a46f506568cb0463c7da70cc198128b93
Yash Anil Borate <yashborate195@gmail.com>
2023-02-24 12:18:01 +0530

    Difference of no

---
+++ sub.java
@@ -0,0 +1,8 @@
+public class sub {
+    public static void main(String args[]){
+        int a=29;
+        int b = 30 ; 
+        int c= b-a;
+        System.out.println("subtraction is : "+ c);
+    }
+}
